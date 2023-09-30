19fbfa0d6b618733e54d942352770f693d3f5c72
Vineet Nadella <nadellav@amazon.com>
2023-09-29 20:55:17 -0400

    Creating toolbar at bottom

---
+++ Clear/AddCoursesView.swift
@@ -0,0 +1,18 @@
+//
+//  AddCoursesView.swift
+//  Clear
+//
+//  Created by Nadella, Vineet on 9/29/23.
+//
+
+import SwiftUI
+
+struct AddCoursesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCoursesView()
+}
